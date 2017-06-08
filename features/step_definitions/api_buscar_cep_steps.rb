12172801be87ee6892e9d93e2161fbc86431a216
luis.felipe <luis.felipe>
2017-06-08 11:11:29 -0300

    refactor

--- features/step_definitions/api_buscar_cep_steps.rb
+++ features/step_definitions/api_buscar_cep_steps.rb
@@ -2,26 +2,16 @@
 #Testar resultado API de CEP dos Correios válido
 
 Dado(/^que eu queira saber os detalhes do endereço de um cep válido "([^"]*)"$/) do |cepValido|
-
 	@response = HTTParty.get("http://correiosapi.apphb.com/cep/#{cepValido}")
-	  
 end
 
 Quando(/^eu receber a resposta da API$/) do
-
 	puts @response.body
 	puts @response.code
-	 
 end
 
 Então(/^devo validar cep, tipo de logradouro, logradouro, bairro, cidade,estado e o response code\.$/) do
-
-	if @response['cep'] == '06342080' && @response['tipoDeLogradouro'] == 'Rua' && @response['logradouro'] == 'Maria José Ferreira' && @response['bairro'] == 'Jardim Helena' && @response['cidade'] == 'Carapicuíba' && @response['estado'] == 'SP' && @response.code == 200
-	puts true
-	else	
-	puts false
-	end
-
+	puts @response.code == 200 && @response['cep'] == '06342080' && @response['tipoDeLogradouro'] == 'Rua' && @response['logradouro'] == 'Maria José Ferreira' && @response['bairro'] == 'Jardim Helena' && @response['cidade'] == 'Carapicuíba' && @response['estado'] == 'SP' 
 end
 ###########################################################################################################################################
 #Cenário 2
@@ -32,11 +22,5 @@ Dado(/^que eu queira saber os detalhes do endereço de um cep inválido "([^"]*)
 end
 
 Então(/^devo validar mensagem de erro e o response code\.$/) do
-
- 	if @response['message'] == 'Endereço não encontrado!' && @response.code == 404
- 	puts true
-	else
- 	puts false
- 	end
- 	
+	puts @response.code == 404 && @response['message'] == 'Endereço não encontrado!'	
 end
