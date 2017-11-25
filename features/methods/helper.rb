78534569913140b2057a1c654374d1ec92046922
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2017-11-25 12:50:25 -0200

    mysql connection test

---
+++ features/methods/helper.rb
@@ -0,0 +1,20 @@
+#teste de conexão BD
+# require 'mysql2'
+# module Teste
+#
+#   class Conex
+#     attr_reader :db
+#
+#     def initialize
+#       @db = Mysql2::Client.new $mysql[$env]
+#     end
+#
+#     def query_teste
+#       db.execute(%Q{SELECT * FROM ACTOR})
+#     end
+#
+#   end
+#
+# end
+#
+# World (Teste)
